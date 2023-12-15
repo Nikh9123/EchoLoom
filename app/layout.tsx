5094ef17e2243557affa41584d3c567cb7ec877d
nikh9123 <satyamnikhil1523@gmail.com>
2023-12-16 02:03:12 +0530

    socketIO backend completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from '@/lib/utils'
 import { ThemeProvider } from "@/components/providers/theme-provider"
 import { ClerkProvider } from '@clerk/nextjs'
 import { ModalProvider } from '@/components/providers/modal-provider'
+import { SocketProvider } from '@/components/providers/socket-provider'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -32,8 +33,10 @@ export default function RootLayout({
             enableSystem={false}
             storageKey='echoloom-theme'
           >
+            <SocketProvider>
             <ModalProvider/>
             {children}
+            </SocketProvider>
           </ThemeProvider>
 
         </body>
